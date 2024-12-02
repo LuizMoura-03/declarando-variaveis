877b360f796071d9952a1015ea0fff36e55baf84
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-02 16:03:33 -0300

    uploading data

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Luiz Fernando";
+        int idade = 30;
+        double altura = 1.75;
+        boolean matriculado = true;
+
+        System.out.println("Dados pessoais :");
+        System.out.println("Nome: "+ nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + " metros");
+        System.out.println("Matriculado em curso ? " + matriculado);
+
+    }
+}
